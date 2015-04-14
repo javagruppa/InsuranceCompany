f06fe8c5f9eff6db4b6660645b1e436bb00b2ce0
zlokex <andre.hovda@gmail.com>
2015-04-14 20:20:59 +0200

    Merge origin/master
    Conflicts:
    	src/uke9oppgave2/Uke9Oppgave2.java

--- src/insurancecompany/claims/Claim.java
+++ src/insurancecompany/claims/Claim.java
@@ -5,11 +5,65 @@
  */
 package insurancecompany.claims;
 
+import people.Customer;
+
+import java.util.Date;
+import java.awt.Image;
 
 /**
  *
  * @author André
  */
 public class Claim {
+    
+    private Customer owner;
+    private Date date;
+    private int claimNumber;
+    private String description;
+    private Image image;
+    private int appraisal;
+    private int disbursement;
+    
+    /**
+     * Empty constructor.
+     */
+    public Claim() {
+        
+    }
+    
+    public Claim(Date date, int claimNumber, String description) {
+        
+    }
+    
+    public void setOwner(Customer owner) {
+        this.owner = owner;
+    }
+    
+    public void setDate(Date date) {
+        this.date = date;
+    }
+    
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    
+    public void setImage(Image image) {
+        this.image = image;
+    }
+    
+    public void setAppraisal(int appraisal) {
+        this.appraisal = appraisal;
+    }
+    
+    public void setDisbursement(int disbursement) {
+        this.disbursement = disbursement;
+    }
+    
+    public void uploadImage() {
+        
+    }
+    
+    
+    
     
 }

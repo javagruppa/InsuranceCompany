9c879a98c1ca1cb39eb8eb350561e0bb7002f66a
carlmollen <Carl.mollen@gmail.com>
2015-04-14 20:21:06 +0200

    Merge origin/master
    Conflicts:
    	src/uke9oppgave2/Uke9Oppgave2.java

--- src/insurancecompany/people/Person.java
+++ src/insurancecompany/people/Person.java
@@ -1,10 +1,11 @@
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 package insurancecompany.people;
-
+import insurancecompany.misc.Address;
 /**
  *
  * @author Carl

98aa5949bea707c6640b3896fb3ba29a6ab880a4
carlmollen <Carl.mollen@gmail.com>
2015-04-15 20:59:15 +0200

    Merge origin/master
    Conflicts:
    	src/uke9oppgave2/Uke9Oppgave2.java

---
+++ src/insurancecompany/vehicles/Vehicle.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package insurancecompany.vehicles;
+
+/**
+ *
+ * @author André
+ */
+public class Vehicle {
+    private int regNumber;
+    
+    public Vehicle(int regNumber){
+        this.regNumber = regNumber;
+    }
+    
+    public String toString(){
+        String s = "";
+        return s;
+    }
+}

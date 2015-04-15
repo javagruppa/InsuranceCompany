45a4615bf9a633efcafdf90fe5ec66d779c835c6
carlmollen <Carl.mollen@gmail.com>
2015-04-15 21:09:18 +0200

    Merge origin/master
    Conflicts:
    	src/uke9oppgave2/Uke9Oppgave2.java

---
+++ src/insurancecompany/people/Customer.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package insurancecompany.people;
+import insurancecompany.misc.Address;
+/**
+ *
+ * @author Carl
+ */
+public class Customer {
+    private int customerID;
+    private Vehicle = null;
+    public static int nextNumber = 100;
+    
+    public Customer(String firstname, String lastname, int personalNumber, String email, Address address) {
+        super(firstname, lastname, personalNumber, email, address);
+        customerID = nextNumber++;
+    }
+    
+    public int getCustomerID(){
+        return customerID;
+    }
+    
+    public String getName(){
+        return firstname + " " + lastname;
+    }
+    
+    public String getEmail(){
+        return email;
+    }
+    
+    public int getPersonalNumber(){
+        return personalNumber;
+    }
+    
+    public String getAddress(){
+        return address.toString();
+    }
+    
+    public void setAddress(Adress newAddress){
+        this.address = newAddress;
+    }
+    
+    public void setEmail(String newMail){
+        this.email = newMail;
+    }
+    
+    
+    public String toString(){
+        String s = "";
+        return s;
+    }
+    
+}

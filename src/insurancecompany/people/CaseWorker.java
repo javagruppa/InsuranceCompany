c2cd9bc4374187a781fc3af83860539264d8c63e
carlmollen <Carl.mollen@gmail.com>
2015-04-15 20:47:28 +0200

    Merge origin/master
    Conflicts:
    	src/uke9oppgave2/Uke9Oppgave2.java

---
+++ src/insurancecompany/people/CaseWorker.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package insurancecompany.people;
+import insurancecompany.misc.Address;
+/**
+ *
+ * @author Carl
+ */
+public class CaseWorker extends Employee {
+
+    public CaseWorker(String firstname, String lastname, int personalNumber, String email, Address address, int employeeNumber) {
+        super(firstname, lastname, personalNumber, email, address, employeeNumber);
+    }
+
+    public String toString(){
+        String s = "";
+        return s;
+    }
+            
+}

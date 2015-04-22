22c4ae6c4c7f5093072aa4e2e43f916cf6ae1e4f
carlmollen <Carl.mollen@gmail.com>
2015-04-22 13:02:25 +0200

    Merge origin/master
    Conflicts:
    	src/uke9oppgave2/Uke9Oppgave2.java

---
+++ src/insurancecompany/datastructures/VehicleList.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package insurancecompany.datastructures;
+import insurancecompany.vehicles.*;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Carl
+ */
+public class VehicleList {
+    private ArrayList<Vehicle> vehicles;
+    
+    public VehicleList() {
+    }
+}

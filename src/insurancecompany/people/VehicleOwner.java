54ca0c6fa980aee47acb5f666fe2626cca7a35c8
carlmollen <Carl.mollen@gmail.com>
2015-04-22 12:58:41 +0200

    Merge origin/master
    Conflicts:
    	src/uke9oppgave2/Uke9Oppgave2.java

--- src/insurancecompany/people/VehicleOwner.java
+++ src/insurancecompany/people/VehicleOwner.java
@@ -14,5 +14,7 @@ public class VehicleOwner extends Person {
         private Vehiclelist vehiclelist;
         
         public VehicleOwner(String firstname, String lastname, int personalNumber, String email, Address address, int phone) {
-        super(firstname, lastname, personalNumber, email, address, phone)
+        super(firstname, lastname, personalNumber, email, address, phone);
+        }
+        
 }

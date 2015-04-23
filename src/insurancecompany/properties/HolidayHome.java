9d80adc0485bdd03ef4c1745c19a29df35e5a520
Sindre Beba <sindre.beba@gmail.com>
2015-04-23 14:37:18 +0200

    Added toString method to HolidayHome.

--- src/insurancecompany/properties/HolidayHome.java
+++ src/insurancecompany/properties/HolidayHome.java
@@ -24,4 +24,26 @@ public class HolidayHome extends Property {
         super(address);
         this.rental = rental;
     }
+    
+    /**
+     * Returns a string representation of this property. The string
+     * representation consists of each field with a short description separated
+     * by a new line.
+     * @return a string representation of this property
+     */
+    @Override
+    public String toString() {
+        // Creates a StringBuilder which will be returned at the end of the 
+        // method.
+        StringBuilder out = new StringBuilder();
+        // Appends the fields with appropriate sentences.
+        out.append(super.toString());
+        if(rental) {
+            out.append("\nUtleie: Ja");
+        } else {
+            out.append("\nUtleie: Nei");
+        }
+        // Returns the string.
+        return out.toString();
+    }
 }

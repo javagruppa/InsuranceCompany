06fe26a49d54e37c423f6fc3a749a302addf9b6a
Sindre Beba <sindre.beba@gmail.com>
2015-04-24 12:10:53 +0200

    Updated Insurance and TravelInsurance after Thursday meeting.

--- src/insurancecompany/insurances/TravelInsurance.java
+++ src/insurancecompany/insurances/TravelInsurance.java
@@ -5,48 +5,51 @@
  */
 package insurancecompany.insurances;
 
-import insurancecompany.people.Customer;
-
 /**
  *
  * @author Sindre
  */
 public class TravelInsurance extends Insurance {
+    /** The coverage of this insurance. */
+    private String coverage;
     /** The insurance area of this insurance. */
     private String insuranceArea;
-    /** The insurance sum of this insurance. */
-    private int insuranceSum;
     
     /**
-     * Constructor.
-     * @param customer the customer who owns this insurance
+     * Constructs a new travel insurance with the specified customerId, 
+     * coverage, excess and insuranceArea. Active is set to true. Date is set 
+     * to the current date.
+     * 
+     * @param customerId the id of the customer who owns this insurance
+     * @param coverage the coverage of this insurance
+     * @param excess the excess of this insurance
      * @param insuranceArea the insurance area of this insurance
-     * @param insuranceSum the insurance sum of this insurance
      */
-    public TravelInsurance(Customer customer, String insuranceArea, 
-            int insuranceSum) {
-        super(customer);
+    public TravelInsurance(int customerId, String coverage, int excess, String 
+            insuranceArea) {
+        super(customerId, excess);
+        this.coverage = coverage;
         this.insuranceArea = insuranceArea;
-        this.insuranceSum = insuranceSum;
     }
     
     /**
      * Returns a string representation of this insurance. The string
      * representation consists of each field with a short description separated
      * by a new line.
+     * 
      * @return a string representation of this insurance
      */
     @Override
     public String toString() {
         // Creates a StringBuilder which will be returned at the end of the 
         // method.
-        StringBuilder out = new StringBuilder();
+        StringBuilder result = new StringBuilder();
         // Appends the fields with appropriate sentences.
-        out.append("REISEFORSIKRING");
-        out.append("\n").append(super.toString());
-        out.append("\nForsikringsområde: ").append(insuranceArea);
-        out.append("\nForsikringssum: ").append(insuranceSum);
+        result.append("REISEFORSIKRING");
+        result.append("\n").append(super.toString());
+        result.append("\nType: ").append(coverage);
+        result.append("\nForsikringsområde: ").append(insuranceArea);
         // Returns the string.
-        return out.toString();
+        return result.toString();
     }
 }

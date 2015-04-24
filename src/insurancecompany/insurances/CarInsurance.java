67267d9922a1a1eb134e7ef1e8d10a495e60a28d
Sindre Beba <sindre.beba@gmail.com>
2015-04-24 12:39:33 +0200

    Updated CarInsurance after Thursday meeting.

--- src/insurancecompany/insurances/CarInsurance.java
+++ src/insurancecompany/insurances/CarInsurance.java
@@ -5,7 +5,6 @@
  */
 package insurancecompany.insurances;
 
-import insurancecompany.people.Customer;
 import insurancecompany.vehicles.Car;
 
 /**
@@ -13,82 +12,65 @@ import insurancecompany.vehicles.Car;
  * @author Sindre
  */
 public class CarInsurance extends VehicleInsurance {
-    /** Whether the car this insurance is for has an alarm or not. */
-    private boolean hasAlarm;
-    /** Whether the car this insurance is for has a garage or not. */
-    private boolean hasGarage;
-    /** Whether a person under 25 years is allowed to drive the car this 
-     * insurance is for. */
-    private boolean youngDriverPermission;
-    /** The car this insurance is for. */
-    private Car car;
     /** The bonus of this insurance. Used to calculate the yearly insurance 
      * premium. */
     private int bonus;
-    /** The price per kilometer of this insurance. Used to calculate the yearly
-     * insurance premium. */
-    private int pricePerKilometer;
+    /** The car this insurance is for. */
+    private Car car;
+    /** Whether the car this insurance is for has a garage or not. */
+    private boolean hasGarage;
     /** The maximum driving length for this insurance. */
-    private int maxDrivingLength;
+    private int maxLength;
+    /** Whether a person under 25 years is allowed to drive the car this 
+     * insurance is for. */
+    private boolean youngDriver;
     
     /**
-     * Constructor.
-     * @param customer the customer who owns this insurance
+     * Constructs a new vehicle insurance with the specified customerId and 
+     * excess. Active is set to true. Date is set to the current date.
+     * 
      * @param car the car this insurance is for
+     * @param customerId the id of the customer who owns this insurance
+     * @param excess the excess of this insurance
+     * @param hasAlarm whether the car this insurance is for has an alarm or not
+     * @param hasGarage whether the car this insurance is for has a garage or 
+     * not
+     * @param maxLength the maximum driving length for this insurance
+     * @param youngDriver whether a person under 25 years is allowed to drive 
+     * the car this insurance is for
      */
-    public CarInsurance(Customer customer, Car car) {
-        super(customer);
-        this.hasAlarm = false;
-        this.hasGarage = false;
-        this.youngDriverPermission = false;
-        this.car = car;
+    public CarInsurance(Car car, int customerId, int excess, boolean hasAlarm,
+            boolean hasGarage, int maxLength, boolean youngDriver) {
+        super(customerId, excess, hasAlarm);
         this.bonus = 0;
-    }
-    
-    /**
-     * Sets a price per kilometer to this insurance.
-     * @param pricePerKilometer the price per kilometer to be set to this 
-     * insurance
-     */
-    public void setPricePerKilometer(int pricePerKilometer) {
-        this.pricePerKilometer = pricePerKilometer;
+        this.car = car;
+        this.hasGarage = hasGarage;
+        this.maxLength = maxLength;
+        this.youngDriver = youngDriver;
     }
     
     /**
      * Returns a string representation of this insurance. The string
      * representation consists of each field with a short description separated
      * by a new line.
+     * 
      * @return a string representation of this insurance
      */
     @Override
     public String toString() {
         // Creates a StringBuilder which will be returned at the end of the 
         // method.
-        StringBuilder out = new StringBuilder();
+        StringBuilder result = new StringBuilder();
         // Appends the fields with appropriate sentences.
-        out.append("BILFORSIKRING");
-        out.append("\n").append(super.toString());
-        out.append("\nPris pr. kilometer: ").append(pricePerKilometer);
-        out.append("\n").append(car.toString());
-        out.append("\nMaksimum kjørelengde: ").append(maxDrivingLength);
-        if(hasAlarm) {
-            out.append("\nBilen har alarm.");
-        } else {
-            out.append("\nBilen har ikke alarm.");
-        }
-        if(hasGarage) {
-            out.append("\nBilen har garasje.");
-        } else {
-            out.append("\nBilen har ikke garasje.");
-        }
-        if(youngDriverPermission) {
-            out.append("\nPersoner under 25 år kan kjøre bilen.");
-        } else {
-            out.append("\nPersoner under 25 år kan ikke kjøre bilen.");
-        }
-        out.append("\nBonus: ").append(bonus).append("%");
+        result.append("BILFORSIKRING");
+        result.append("\n").append(super.toString());
+        result.append("\nGarasje: ").append(hasGarage ? "Ja" : "Nei");
+        result.append("\nKan personer under 25 kjøre bilen: ").append(youngDriver 
+                ? "Ja" : "Nei");
+        result.append("\nMaksimum kjørelengde: ").append(maxLength);
+        result.append("\nBonus: ").append(bonus).append("%");
         // Returns the string.
-        return out.toString();
+        return result.toString();
     }
     
     /**

b7b6cd84aade05ae86db02ccd7acf59c8c5a0b47
carlmollen <Carl.mollen@gmail.com>
2015-04-28 21:28:38 +0200

    Merge origin/master
    Conflicts:
    	src/uke9oppgave2/Uke9Oppgave2.java

--- src/insurancecompany/people/Person.java
+++ src/insurancecompany/people/Person.java
@@ -19,7 +19,15 @@ public class Person {
     protected int phone;
     
     
-    
+    /**
+     * Constructs a person-object with specified parameters
+     * @param firstname the first name of the person
+     * @param lastname the last name of the person
+     * @param personalNumber the persons personal number
+     * @param email the email address of the person
+     * @param address the address of the person, as an object
+     * @param phone the phone number of the person
+     */
     public Person(String firstname, String lastname, int personalNumber, String email, Address address, int phone) {
         this.firstname = firstname;
         this.lastname = lastname;
@@ -30,26 +38,51 @@ public class Person {
         
     }
     
+    /**
+     * Returns the persons first name
+     * @return the first name as a string
+     */
     public String getFirstname(){
         return firstname;
     }
-    
+    /**
+     * Returns the persons last name
+     * @return the last name as a string
+     */
     public String getLastname(){
         return lastname;
     }
     
+    /**
+     * Returns the personal number of the person.
+     * @return the personal number as an integer
+     */
     public int getPersonalNumber(){
         return personalNumber;
     }
     
+    /**
+     * Returns the persons email address
+     * @return the email address as a string
+     */
     public String getEmail(){
         return email;
     }
     
+    /**
+     * Return the toString of the address object for this person
+     * @return the address-objects toString for the specific person
+     */
     public String getAddressString(){
         return address.toString();
     }
     
+    /**
+     * Returns a string representation of the person.
+     * All fields included, as well as the toString from the per address object.
+     * 
+     * @return a string representation of this person
+     */
     public String toString() {
         String s = firstname + "\n" + lastname + "\n" + "Fødselsnr: " + personalNumber +
                 "/nEpost-adresse: " + email + "\n" + address.toString();

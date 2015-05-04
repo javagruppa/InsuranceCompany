94c5b8e5ae6d6fb65482d0e14c5b61a33fee7391
zlokex <andre.hovda@gmail.com>
2015-05-04 16:12:36 +0200

    Added GuiAdmin, CustomerRegistration og oppdateret registerGui

--- src/insurancecompany/gui/CustomerRegistration.java
+++ src/insurancecompany/gui/CustomerRegistration.java
@@ -5,42 +5,186 @@
  */
 package insurancecompany.gui;
 
+import javafx.application.Application;
+import static javafx.application.Application.launch;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 /**
  *
  * @author André
  */
-public class CustomerRegistration {
+public class CustomerRegistration extends Application {
     
     private Scene scene;
+    private GridPane mainPane;
     
-    private TextField nameField;
-    private TextField nameField;
-    private TextField nameField;
     
-    protected int personalNumber;
-    protected String firstname;
-    protected String lastname;
-    protected Address address;
-    protected String email;
-    protected int phone;
+    private TextField personalNumberField;
+    private TextField firstNameField;
+    private TextField lastNameField;    
+    private TextField streetField;
+    private TextField zipCodeField;
+    private TextField cityField;
+    private TextField emailField;
+    private TextField phoneField;
+    private Button registerButton;
+    
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+    public void start(Stage stage) {
+        show(stage);
+    }
     
     public CustomerRegistration() {
-        GridPane grid = new GridPane();
-        grid.setAlignment(Pos.CENTER);
-        grid.setHgap(10);
-        grid.setVgap(10);
+        mainPane = new GridPane();
+        mainPane.setAlignment(Pos.TOP_LEFT);
+        mainPane.setHgap(10);
+        mainPane.setVgap(10);
         
-        scene = new Scene(grid, 300, 275);
+        scene = new Scene(getMainPane(), 300, 275);
         
-        Label customerL = new Label("Kunde");
-        customerField = new TextField();
+        Text registerT = new Text("Registrer kunde");
+        mainPane.add(registerT, 1, 1);
+        Label personalNumber = new Label("Personnummer:");
+        mainPane.add(personalNumber, 1, 2);
+        personalNumberField = new TextField();
+        mainPane.add(personalNumberField, 2, 2);
+        Label firstName = new Label("Fornavn:");
+        mainPane.add(firstName, 1, 3);
+        firstNameField = new TextField();
+        mainPane.add(firstNameField, 2, 3);
+        Label lastName = new Label("Etternavn:");
+        mainPane.add(lastName, 1, 4);
+        lastNameField = new TextField();
+        mainPane.add(lastNameField, 2, 4);
+        Text adress = new Text("Adresse");
+        mainPane.add(adress, 1, 5);
+        Label street = new Label("Gate:");
+        mainPane.add(street, 1, 6);
+        streetField = new TextField();
+        mainPane.add(streetField, 2, 6);
+        Label zipCode = new Label("Postboks:");
+        mainPane.add(zipCode, 1, 7);
+        zipCodeField = new TextField();
+        mainPane.add(zipCodeField, 2, 7);
+        Label city = new Label("By:");
+        mainPane.add(city, 1, 8);
+        cityField = new TextField();
+        mainPane.add(cityField, 2, 8);
+        Text contact = new Text("Kontakt");
+        mainPane.add(contact, 1, 9);
+        Label email = new Label("E-post:");
+        mainPane.add(email, 1, 10);
+        emailField = new TextField();
+        mainPane.add(emailField, 2, 10);
+        Label phone = new Label("Telefonnummer:");
+        mainPane.add(phone, 1, 11);
+        phoneField = new TextField();
+        mainPane.add(phoneField, 2, 11);
+        registerButton = new Button("Registrer");
+        HBox hbBtn = new HBox();
+        hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
+        hbBtn.getChildren().add(registerButton);
+        mainPane.add(hbBtn, 2, 12);
+        
+    }
+    
+    public void show(Stage stage) {
+        stage.setTitle("Kunderegistrering");
+        stage.setScene(getScene());
+        stage.show();
+    }
+    
+    public Pane getPane() {
+        return getMainPane();
+    }
+
+    /**
+     * @return the scene
+     */
+    public Scene getScene() {
+        return scene;
+    }
+
+    /**
+     * @return the mainPane
+     */
+    public GridPane getMainPane() {
+        return mainPane;
+    }
+
+    /**
+     * @return the personalNumberField
+     */
+    public TextField getPersonalNumberField() {
+        return personalNumberField;
+    }
+
+    /**
+     * @return the firstNameField
+     */
+    public TextField getFirstNameField() {
+        return firstNameField;
+    }
+
+    /**
+     * @return the lastNameField
+     */
+    public TextField getLastNameField() {
+        return lastNameField;
+    }
+
+    /**
+     * @return the streetField
+     */
+    public TextField getStreetField() {
+        return streetField;
+    }
+
+    /**
+     * @return the zipCodeField
+     */
+    public TextField getZipCodeField() {
+        return zipCodeField;
+    }
+
+    /**
+     * @return the cityField
+     */
+    public TextField getCityField() {
+        return cityField;
+    }
+
+    /**
+     * @return the emailField
+     */
+    public TextField getEmailField() {
+        return emailField;
+    }
+
+    /**
+     * @return the phoneField
+     */
+    public TextField getPhoneField() {
+        return phoneField;
+    }
+
+    /**
+     * @return the registerButton
+     */
+    public Button getRegisterButton() {
+        return registerButton;
     }
     
 }
--- src/insurancecompany/gui/GuiAdmin.java
+++ src/insurancecompany/gui/GuiAdmin.java
@@ -5,14 +5,150 @@
  */
 package insurancecompany.gui;
 
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
 /**
  *
  * @author André
  */
-public class GuiAdmin {
+public class GuiAdmin extends Application {
     
+    
+    private Scene scene;
+    private BorderPane mainPane;
+    private Button registerButton;
+    private Button searchButton;
+    private Button statisticsButton;
+    private Button logOutButton;
+    private Button exitButton;
+    private Text userStatusText;
+            
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        show(stage);
+    }
+    
+    public void show(Stage stage) {
+        stage.setTitle("Kunderegistrering");
+        stage.setScene(getScene());
+        stage.show();
+    }
     
     public GuiAdmin() {
-        GridPane grid = new GridPane();
+        mainPane = new BorderPane();
+        VBox vbox = new VBox();
+        vbox.getChildren().addAll(createToolBar(), createStatusBar());
+        mainPane.setTop(vbox);
+        scene = new Scene(getMainPane(), 900, 600);
+        scene.getStylesheets().add("stylesheet.css");
     }
+    
+    public HBox createToolBar() {
+        HBox hbox = new HBox();
+        hbox.setStyle("-fx-background-color: #336699;");
+        registerButton = new Button("Registrer");
+        getRegisterButton().setId("mainToolbarButton");
+        searchButton = new Button("Søk");
+        getSearchButton().setId("mainToolbarButton");
+        statisticsButton = new Button("Statistikk");
+        getStatisticsButton().setId("mainToolbarButton");
+        
+        logOutButton = new Button("Logg ut");
+        getLogOutButton().setId("mainToolbarButton");
+        exitButton = new Button("Avslutt");
+        getExitButton().setId("mainToolbarButton");
+        
+        HBox hbox1 = new HBox();
+        hbox1.getChildren().addAll(getLogOutButton(), getExitButton());
+        hbox1.setAlignment(Pos.CENTER_RIGHT);
+        HBox.setHgrow(hbox1, Priority.ALWAYS);
+        
+        hbox.getChildren().addAll(getRegisterButton(), getSearchButton(), getStatisticsButton());
+        hbox.getChildren().add(hbox1);
+        
+        return hbox;
+    }
+    
+    public HBox createStatusBar() {
+        HBox hbox = new HBox();
+        hbox.setPadding(new Insets(5, 0, 5, 0));
+        hbox.setStyle("-fx-background-color: #FFFFFF;");
+        
+        userStatusText = new Text("Logget inn som Admin. Ansattnr: 10000000");
+        hbox.getChildren().add(getUserStatusText());
+        return hbox;
+    }
+
+    /**
+     * @return the scene
+     */
+    public Scene getScene() {
+        return scene;
+    }
+
+    /**
+     * @return the mainPane
+     */
+    public BorderPane getMainPane() {
+        return mainPane;
+    }
+
+    /**
+     * @return the registerButton
+     */
+    public Button getRegisterButton() {
+        return registerButton;
+    }
+
+    /**
+     * @return the searchButton
+     */
+    public Button getSearchButton() {
+        return searchButton;
+    }
+
+    /**
+     * @return the statisticsButton
+     */
+    public Button getStatisticsButton() {
+        return statisticsButton;
+    }
+
+    /**
+     * @return the logOutButton
+     */
+    public Button getLogOutButton() {
+        return logOutButton;
+    }
+
+    /**
+     * @return the exitButton
+     */
+    public Button getExitButton() {
+        return exitButton;
+    }
+
+    /**
+     * @return the userStatusText
+     */
+    public Text getUserStatusText() {
+        return userStatusText;
+    }
+    
 }

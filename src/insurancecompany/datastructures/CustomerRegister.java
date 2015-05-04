dbe79cb415aafa81b85400a70b8746b8f4a670a1
zlokex <andre.hovda@gmail.com>
2015-05-04 17:46:50 +0200

    implements Serializable

--- src/insurancecompany/datastructures/CustomerRegister.java
+++ src/insurancecompany/datastructures/CustomerRegister.java
@@ -7,6 +7,7 @@ package insurancecompany.datastructures;
 
 import insurancecompany.people.*;
 import insurancecompany.misc.Address;
+import java.io.Serializable;
 
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -15,7 +16,7 @@ import java.util.Iterator;
  *
  * @author André
  */
-public class CustomerRegister {
+public class CustomerRegister implements Serializable {
     
     private ArrayList<Customer> customers;
     

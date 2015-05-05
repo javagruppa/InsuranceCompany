4b27df29d5edaf68f5fae7716d114aeb8a863058
carlmollen <Carl.mollen@gmail.com>
2015-05-05 10:15:20 +0200

    Merge origin/master
    Conflicts:
    	src/uke9oppgave2/Uke9Oppgave2.java

--- src/insurancecompany/people/Admin.java
+++ src/insurancecompany/people/Admin.java
@@ -12,12 +12,25 @@ import insurancecompany.misc.Address;
  */
 public class Admin extends Employee {
 
+    /**
+     * Constructs an Admin-object with the specified parameters
+     * @param firstname first name of person
+     * @param lastname last name of person
+     * @param personalNumber personal number of person
+     * @param email e-mail address of the person
+     * @param address address of the person
+     * @param phone  phone number for the person
+     */
     public Admin(String firstname, String lastname, long personalNumber, String email, Address address, String phone) {
         super(firstname, lastname, personalNumber, email, address, phone);
     }
 
+    /**
+     * Returns a string representation for the Admin
+     * @return a string of the admin
+     */
     public String toString(){
-        String s = "";
+        String s = "String tekst for Admin toString ikke lagt inn";
         return s;
     }
             

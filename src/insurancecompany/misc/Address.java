af01d50094b70042be4df2620585a28353dd97c6
zlokex <andre.hovda@gmail.com>
2015-05-06 09:29:10 +0200

    Removed equals method

--- src/insurancecompany/misc/Address.java
+++ src/insurancecompany/misc/Address.java
@@ -11,17 +11,18 @@ package insurancecompany.misc;
  * @author Carl
  */
 public class Address {
-    // Fields are final as they are used in the overriden equals method, and specifically to identify Property objects
-    private final String street; // Includes street name, number and letter.
-    private final int zipCode;
-    private final String city;
+    private String street; // Includes street name, number and letter.
+    private int zipCode;
+    private String city;
     
     public Address(String street, int zipCode, String city) {
         this.street = street;
         this.zipCode = zipCode;
         this.city = city;
     }
-     
+    
+
+    
     public String getStreet() {
         return street;
     }
@@ -39,23 +40,4 @@ public class Address {
             return s;
     }
     
-        /**
-     * Indicates whether some other address is equal to this one. The result 
-     * is true if and only if the argument is not null and is an Address 
-     * object that contains the same insuranceId value as this object.
-     * 
-     * @param obj the object to compare with
-     * @return true if the objects are the same; false otherwise
-     */
-    @Override
-    public boolean equals(Object obj) {
-        if(obj != null && obj instanceof Address) {
-            Address other = (Address) obj;
-            return (getStreet() == other.getStreet()) 
-                    && (getZipCode() == other.getZipCode())
-                    && (getCity() == other.getCity());
-        } else {
-            return false;
-        }   
-    }
 }

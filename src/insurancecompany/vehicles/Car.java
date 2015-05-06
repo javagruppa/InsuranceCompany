a380ceacd3f95ce1d42937cf1113aaa94bfd6d4f
Sindre Beba <sindre.beba@gmail.com>
2015-05-06 12:25:54 +0200

    Updated javadoc for Car and Boat.

--- src/insurancecompany/vehicles/Car.java
+++ src/insurancecompany/vehicles/Car.java
@@ -11,6 +11,18 @@ package insurancecompany.vehicles;
  * @author Sindre
  */
 public class Car extends Vehicle {
+    /**
+     * Constructs a new car of the specified brand and model, and with the
+     * specified engine effect, registration number and registration year. The
+     * owner of this car has the specified personal number.
+     * 
+     * @param brand the brand of this car
+     * @param engineEffect the effect of the engine of this car
+     * @param model the model of this car
+     * @param personalNumber the personal number of the owner of this car
+     * @param regNumber the registration number of this car
+     * @param regYear the registration year of this car
+     */
     public Car(String brand, int engineEffect, String model, 
             String personalNumber, String regNumber, int regYear){
         super(brand, engineEffect, model, personalNumber, regNumber, regYear);

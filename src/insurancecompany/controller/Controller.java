c2e7783078bdf40021b640d6ab350f463b2158ac
zlokex <andre.hovda@gmail.com>
2015-05-07 18:39:57 +0200

    Lots of changes Controller

--- src/insurancecompany/controller/Controller.java
+++ src/insurancecompany/controller/Controller.java
@@ -11,7 +11,6 @@ import insurancecompany.model.people.Admin;
 import insurancecompany.model.people.CaseWorker;
 import insurancecompany.model.datastructures.InsuranceRegister;
 import insurancecompany.model.people.Customer;
-import insurancecompany.model.datastructures.VehicleRegister;
 import insurancecompany.model.datastructures.EmployeeRegister;
 import insurancecompany.model.people.Employee;
 import insurancecompany.model.datastructures.CustomerRegister;
@@ -19,7 +18,7 @@ import insurancecompany.model.properties.Address;
 import insurancecompany.view.AdminView;
 import insurancecompany.view.register.BoatInsuranceRegistration;
 import insurancecompany.view.register.HolidayHomeInsuranceRegistration;
-import insurancecompany.view.RegisterPane;
+import insurancecompany.view.RegisterView;
 import insurancecompany.view.register.HomeInsuranceRegistration;
 import insurancecompany.view.register.TravelInsuranceRegistration;
 import insurancecompany.view.register.CustomerRegistration;
@@ -51,10 +50,10 @@ public class Controller {
     private static final String customerFileName = "src/customerRegister.dta";
     
     // Views:
-    private LoginView login;
-    private RegisterPane registerPane;
-    private CustomerRegistration cReg;
-    private BoatInsuranceRegistration biReg;
+    private LoginView loginView;
+    private RegisterView registerView;
+    private CustomerRegistration customerRegistration;
+    private BoatInsuranceRegistration boatInsuranceRegistration;
     private HomeInsuranceRegistration hiReg;
     private HolidayHomeInsuranceRegistration hhiReg;
     private TravelInsuranceRegistration tiReg;
@@ -64,7 +63,6 @@ public class Controller {
     private EmployeeRegister employees;
     private CustomerRegister customers;
     private InsuranceRegister insurances;
-    private VehicleRegister vehicles;
     private ClaimRegister claims;
     
     public Controller()  {
@@ -72,32 +70,30 @@ public class Controller {
         employees = new EmployeeRegister();
         customers = new CustomerRegister();
         insurances = new InsuranceRegister();
-        vehicles = new VehicleRegister();
         claims = new ClaimRegister();
         
-        login = new LoginView();
-        registerPane = new RegisterPane();
-        cReg = new CustomerRegistration();
-        biReg = new BoatInsuranceRegistration();
+        loginView = new LoginView();
+        registerView = new RegisterView();
+        customerRegistration = new CustomerRegistration();
+        boatInsuranceRegistration = new BoatInsuranceRegistration();
         hiReg = new HomeInsuranceRegistration();
         hhiReg = new HolidayHomeInsuranceRegistration();
         tiReg = new TravelInsuranceRegistration();
         guiAdmin = new AdminView();
-        login.getLoginBtn().setOnAction(this::loginBtnEventHandler);
-        guiAdmin.getRegisterButton().setOnAction(this::registerPaneEventHandler);
-        registerPane.getCustomerButton().setOnAction(this::registerCustomerPaneEventHandler);
-        registerPane.getBoatInsuranceButton().setOnAction(this::registerBoatInsurancePaneEventHandler);
-        registerPane.getHomeInsuranceButton().setOnAction(this::registerHomeInsurancePaneEventHandler);
-        registerPane.getHolidayHomeInsuranceButton().setOnAction(this::registerHolidayHomeInsurancePaneEventHandler);
-        registerPane.getTravelInsuranceButton().setOnAction(this::registerTravelInsurancePaneEventHandler);
-        cReg.getRegisterButton().setOnAction(this::registerCustomerEventHandler);
+        loginView.getLoginBtn().setOnAction(this::loginBtnEventHandler);
+        registerView.getCustomerButton().setOnAction(this::registerCustomerPaneEventHandler);
+        registerView.getBoatInsuranceButton().setOnAction(this::registerBoatInsurancePaneEventHandler);
+        //registerPane.getHomeInsuranceButton().setOnAction(this::registerHomeInsurancePaneEventHandler);
+        //registerPane.getHolidayHomeInsuranceButton().setOnAction(this::registerHolidayHomeInsurancePaneEventHandler);
+        registerView.getTravelInsuranceButton().setOnAction(this::registerTravelInsurancePaneEventHandler);
+        customerRegistration.getRegisterButton().setOnAction(this::registerCustomerEventHandler);
     }
     
     public void initGui() {
-        Pane pane = registerPane.getMainPane();
+        Pane pane = registerView.getMainPane();
         guiAdmin.getMainPane().setCenter(pane);
-        Pane pane2 = cReg.getMainPane();
-        registerPane.getMainPane().setCenter(pane2);
+        Pane pane2 = customerRegistration.getMainPane();
+        registerView.getMainPane().setCenter(pane2);
     }
     
     public void show(Stage stage) {
@@ -110,20 +106,20 @@ public class Controller {
         boolean ok = true;
         String output = "";
         
-        String personalNumberS = cReg.getPersonalNumberField().getText();
-        String firstName = cReg.getFirstNameField().getText();
-        String lastName = cReg.getLastNameField().getText();
-        String street = cReg.getStreetField().getText();
-        String zipCodeS = cReg.getZipCodeField().getText();
-        String city = cReg.getCityField().getText();
-        String email = cReg.getEmailField().getText();
-        String phone = cReg.getPhoneField().getText();
+        String personalNumberS = customerRegistration.getPersonalNumberField().getText();
+        String firstName = customerRegistration.getFirstNameField().getText();
+        String lastName = customerRegistration.getLastNameField().getText();
+        String street = customerRegistration.getStreetField().getText();
+        String zipCodeS = customerRegistration.getZipCodeField().getText();
+        String city = customerRegistration.getCityField().getText();
+        String email = customerRegistration.getEmailField().getText();
+        String phone = customerRegistration.getPhoneField().getText();
         
         if (personalNumberS.equals("")) {
             Label message = new Label();
             message.setText("*");
             message.setTextFill(Color.rgb(210, 39, 30));
-            cReg.setPersonalNumberMessage(message);
+            customerRegistration.setPersonalNumberMessage(message);
         }
  
         long personalNumber = 0;
@@ -143,9 +139,9 @@ public class Controller {
         
         Customer customer = new Customer(firstName, lastName, personalNumber, email, adress, phone);
         
+        
         int customerId = customer.getCustomerId();
         System.out.println(customerId);
-        
         boolean ok1 = customers.addCustomer(customer);
         
         //Customer test = customers.findCustomerById(customerId);
@@ -160,19 +156,19 @@ public class Controller {
     
     /*private void registerBoatInsuranceEventHandler(ActionEvent e) {
         // Collects information about the boat.
-        String regNumber = biReg.getRegNumberField().getText();
-        String effectS = biReg.getEffectField().getText();
-        String lengthS = biReg.getLengthField().getText();
-        String engineType = biReg.getEngineTypeField().getText();
-        String yearS = biReg.getYearField().getText();
-        String make = biReg.getMakeField().getText();
-        String model = biReg.getModelField().getText();
+        String regNumber = boatInsuranceRegistration.getRegNumberField().getText();
+        String effectS = boatInsuranceRegistration.getEffectField().getText();
+        String lengthS = boatInsuranceRegistration.getLengthField().getText();
+        String engineType = boatInsuranceRegistration.getEngineTypeField().getText();
+        String yearS = boatInsuranceRegistration.getYearField().getText();
+        String make = boatInsuranceRegistration.getMakeField().getText();
+        String model = boatInsuranceRegistration.getModelField().getText();
         
         // Collects information about the customer and the insurance.
-        String customerId = biReg.getCustomerIdField().getText();
-        String coverageS = biReg.getCoverageField().getText();
-        String excessS = biReg.getExcessField().getText();
-        boolean hasAlarm = biReg.getHasAlarmCheckBox().isSelected();
+        String customerId = boatInsuranceRegistration.getCustomerIdField().getText();
+        String coverageS = boatInsuranceRegistration.getCoverageField().getText();
+        String excessS = boatInsuranceRegistration.getExcessField().getText();
+        boolean hasAlarm = boatInsuranceRegistration.getHasAlarmCheckBox().isSelected();
         
         // Converts strings to integers.
         int effect;
@@ -216,38 +212,35 @@ public class Controller {
         //new Boat(int regNumber, int effect, int length, String engineType, int year, String make, String model)
     }*/
     
-    private void registerPaneEventHandler(ActionEvent e) {
-        Pane pane = registerPane.getMainPane();
-        guiAdmin.getMainPane().setCenter(pane);       
-    }
     
     private void registerCustomerPaneEventHandler(ActionEvent e) {
-        Pane pane = cReg.getMainPane();
-        registerPane.getMainPane().setCenter(pane); 
+        Pane pane = customerRegistration.getMainPane();
+        registerView.getMainPane().setCenter(pane); 
     }
     
     private void registerBoatInsurancePaneEventHandler(ActionEvent e) {
-        Pane pane = biReg.getMainPane();
-        registerPane.getMainPane().setCenter(pane); 
+        Pane pane = boatInsuranceRegistration.getMainPane();
+        registerView.getMainPane().setCenter(pane); 
 
     }
-    
+    /*
     private void registerHomeInsurancePaneEventHandler(ActionEvent e) {
         Pane pane = hiReg.getMainPane();
-        registerPane.getMainPane().setCenter(pane); 
-        selectedButtonStyleLower(registerPane.getHomeInsuranceButton());
+        registerView.getMainPane().setCenter(pane); 
+        selectedButtonStyleLower(registerView.getHomeInsuranceButton());
     }
     
     private void registerHolidayHomeInsurancePaneEventHandler(ActionEvent e) {
         Pane pane = hhiReg.getMainPane();
-        registerPane.getMainPane().setCenter(pane); 
-        selectedButtonStyleLower(registerPane.getHolidayHomeInsuranceButton());
+        registerView.getMainPane().setCenter(pane); 
+        selectedButtonStyleLower(registerView.getHolidayHomeInsuranceButton());
     }
+    * */
     
     private void registerTravelInsurancePaneEventHandler(ActionEvent e) {
         Pane pane = tiReg.getMainPane();
-        registerPane.getMainPane().setCenter(pane); 
-        //selectedButtonStyleLower(registerPane.getTravelInsuranceButton());
+        registerView.getMainPane().setCenter(pane); 
+        //selectedButtonStyleLower(registerView.getTravelInsuranceButton());
     }
     
 
@@ -255,7 +248,7 @@ public class Controller {
     
     private void loginBtnEventHandler(ActionEvent e) {
         
-        String userName = login.getUserTextField().getText();
+        String userName = loginView.getUserTextField().getText();
         int employeeId = Integer.parseInt(userName);
         Employee employee = employees.getEmployeeById(employeeId);
         if (employee instanceof Admin) {

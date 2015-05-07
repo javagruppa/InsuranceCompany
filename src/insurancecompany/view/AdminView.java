91c1e632e7cfc52029051aab8026b438192a245d
zlokex <andre.hovda@gmail.com>
2015-05-07 21:12:09 +0200

    Big update to AdminView

--- src/insurancecompany/view/AdminView.java
+++ src/insurancecompany/view/AdminView.java
@@ -14,9 +14,12 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.input.MouseButton;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
@@ -29,14 +32,26 @@ import javafx.stage.StageStyle;
  */
 public class AdminView extends Application {
     
+    private Stage primaryStage;
     
     private RegisterView registerView;
     private CustomerRegistration customerRegistration;
     private BoatInsuranceRegistration boatInsuranceRegistration;
     private TravelInsuranceRegistration travelInsuranceRegistration;
     
+    private double xOffset = 0;
+    private double yOffset = 0;
+    //private double dx = 0;
+    //private double dy = 0;
+    //private boolean resizeBottom = false;
+    
     private Scene scene;
     private BorderPane mainPane;
+    
+    private Pane toolBarPane;
+    private Pane statusBarPane;
+    private Pane centerPane;
+    
     private Button registerTabButton;
     private Button searchTabButton;
     private Button statisticsTabButton;
@@ -49,29 +64,41 @@ public class AdminView extends Application {
     private GridPane travelInsurancePane;     
     
     public AdminView() {
-        mainPane = new BorderPane();
-        VBox vbox = new VBox();
-        vbox.getChildren().addAll(createToolBar(), createStatusBar());
-        mainPane.setTop(vbox);
-        scene = new Scene(getMainPane(), 900, 600);
-        scene.getStylesheets().add("insurancecompany/resources/stylesheet.css");
         initializeViews();
         initializeEventHandlers();
+        scene = new Scene(mainPane, 900, 600);
+        scene.getStylesheets().add("insurancecompany/resources/stylesheet.css");
+        
     }
     public void start(Stage stage) throws Exception {
         show(stage);
     }
     
     public void show(Stage stage) {
-        //stage.initStyle(StageStyle.UNDECORATED);
-        stage.setTitle("Kunderegistrering");
-        stage.setScene(getScene());
-        stage.show();
+        primaryStage = stage;
+        primaryStage.setFullScreen(true);
+        primaryStage.initStyle(StageStyle.UNDECORATED);
+        primaryStage.setTitle("Kunderegistrering");
+        primaryStage.setScene(getScene());
+        primaryStage.show();
     }
     
     private void initializeViews() {
+        mainPane = new BorderPane();
+        VBox vbox = new VBox();
+        toolBarPane = createToolBar();
+        statusBarPane = createStatusBar();
+        vbox.getChildren().addAll(toolBarPane, statusBarPane);
+        
         registerView = new RegisterView();
         registerPane = registerView.getMainPane();
+        
+        mainPane.setTop(vbox);
+        centerPane = registerPane;
+        mainPane.setCenter(centerPane);
+        
+        
+        
     }
     
     private void initializeEventHandlers() {
@@ -83,6 +110,34 @@ public class AdminView extends Application {
         exitButton.setOnAction((event) -> {
             Platform.exit();     
         });
+        
+        toolBarPane.setOnMouseClicked((event) -> {
+            if(event.getButton().equals(MouseButton.PRIMARY)){
+            if(event.getClickCount() == 2){
+                if (primaryStage.isFullScreen())
+                    primaryStage.setFullScreen(false);
+                else if (!primaryStage.isFullScreen()) {
+                    primaryStage.setFullScreen(true);
+                }
+            }
+        }
+            
+        });
+        
+        toolBarPane.setOnMousePressed((event) -> {
+            if (!primaryStage.isFullScreen()) {
+                xOffset = event.getSceneX();
+                yOffset = event.getSceneY();
+            }
+        });
+        
+        toolBarPane.setOnMouseDragged((event) -> {
+            if (!primaryStage.isFullScreen()) {
+                primaryStage.setX(event.getScreenX() - xOffset);
+                primaryStage.setY(event.getScreenY() - yOffset);
+            }
+        });        
+        
     }
     
     

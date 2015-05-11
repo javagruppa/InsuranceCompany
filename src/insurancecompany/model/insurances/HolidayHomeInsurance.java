26bfb76af31abc752b9bbdac5cfac68276141ae2
carlmollen <Carl.mollen@gmail.com>
2015-05-11 14:09:35 +0200

    holidayhomeinsurance: endret så denne kun gjelder husforsikring

--- src/insurancecompany/model/insurances/HolidayHomeInsurance.java
+++ src/insurancecompany/model/insurances/HolidayHomeInsurance.java
@@ -6,7 +6,7 @@
 package insurancecompany.model.insurances;
 
 import insurancecompany.model.properties.HolidayHome;
-import insurancecompany.misc.hometypes.HomeType;
+import insurancecompany.misc.hometypes.HolidayHomeType;
 import java.io.Serializable;
 /**
  *
@@ -18,24 +18,23 @@ public class HolidayHomeInsurance extends PropertyInsurance implements Serializa
     /** The holiday home this insurance is for. */
     private HolidayHome holidayHome;
     /** The type of house this insurance is for. */
-    private HomeType hometype;
+    private HolidayHomeType holidayhometype;
     
     /**
-     * Constructs a new holiday home insurance with the specified 
-     * buildingAmount, contentAmount, customerId, excess and holidayHome. 
+     *  * Constructs a new holiday home insurance with the specified contentAmount, 
+     * customerId, excess, holidayhometype, holidayhomecoverage and holidayHome. 
      * Active is set to true. Date is set to the current date. InsuranceId is 
      * automatically set to nextInsuranceId.
      * 
-     * @param buildingAmount the building insurance amount of this insurance
-     * @param contentAmount the content insurance amount of this insurance
-     * @param customerId the id of the customer who owns this insurance
+     * @param type the type of holidayhouse this insurance is for
+     * @param customerId the customer that has this insurance
      * @param excess the excess of this insurance
-     * @param holidayHome the holiday home this insurance is for
+     * @param holidayHome  the holidayhome that is insured
      */
-    public HolidayHomeInsurance(int buildingAmount, int contentAmount, 
-            int customerId, int excess, HolidayHome holidayHome) {
-        super(buildingAmount, contentAmount, customerId, excess);
-        this.holidayHome = holidayHome;                
+    public HolidayHomeInsurance(HolidayHomeType type, int customerId, int excess, HolidayHome holidayHome) {
+        super(customerId, excess);
+        this.holidayHome = holidayHome;
+        this.holidayhometype = type;
     }
     
     /**

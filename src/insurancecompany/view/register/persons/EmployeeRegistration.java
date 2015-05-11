48da45c0075cfd121b8e440009cdbc42e3d5641d
zlokex <andre.hovda@gmail.com>
2015-05-11 23:33:37 +0200

    Added postion to employeereg

--- src/insurancecompany/view/register/persons/EmployeeRegistration.java
+++ src/insurancecompany/view/register/persons/EmployeeRegistration.java
@@ -5,13 +5,11 @@
  */
 package insurancecompany.view.register.persons;
 
-import javafx.application.Application;
-import static javafx.application.Application.launch;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
@@ -23,11 +21,12 @@ import javafx.stage.Stage;
  *
  * @author André
  */
-public class EmployeeRegistration extends Application {
+public class EmployeeRegistration {
     
     private Scene scene;
     private GridPane mainPane;
     
+    private ComboBox positionCombo;
     private TextField personalNumberField;
     private TextField firstNameField;
     private TextField lastNameField;    
@@ -37,27 +36,24 @@ public class EmployeeRegistration extends Application {
     private TextField emailField;
     private TextField phoneField;
     
-    private Label personalNumberMessage;
-    private Label firstNameMessage;
-    private Label lastNameMessage;    
-    private Label streetMessage;
-    private Label zipCodeMessage;
-    private Label cityMessage;
-    private Label emailMessage;
-    private Label phoneMessage;
+    private Text personalNumberMessage;
+    private Text firstNameMessage;
+    private Text lastNameMessage;    
+    private Text streetMessage;
+    private Text zipCodeMessage;
+    private Text cityMessage;
+    private Text emailMessage;
+    private Text phoneMessage;
     
     private Button registerButton;
     
     private Text resultText;
     
-    public static void main(String[] args) {
-        launch(args);
-    }
-    
     public void start(Stage stage) {
         show(stage);
     }
     
+    // TODO: Set fixed size for Text message fields:(or one of them)
     public EmployeeRegistration() {
         mainPane = new GridPane();
         mainPane.setAlignment(Pos.CENTER);
@@ -68,65 +64,86 @@ public class EmployeeRegistration extends Application {
         scene = new Scene(getMainPane(), 300, 275);
         
         Text registerT = new Text("Registrer ansatt");
-        mainPane.add(registerT, 1, 1);
+        Label position = new Label("Posisjon");
+        positionCombo = new ComboBox();
+        positionCombo.getItems().addAll("Kundebehandler", "Saksbehandler", "Administrator");
+        
         Label personalNumber = new Label("Personnummer:");
-        mainPane.add(personalNumber, 1, 2);
         personalNumberField = new TextField();
-        mainPane.add(personalNumberField, 2, 2);
-        personalNumberMessage = new Label();
-        mainPane.add(personalNumberMessage, 3, 2);
+        personalNumberMessage = new Text();
         Label firstName = new Label("Fornavn:");
-        mainPane.add(firstName, 1, 3);
         firstNameField = new TextField();
-        mainPane.add(firstNameField, 2, 3);
-        firstNameMessage = new Label();
-        mainPane.add(firstNameMessage, 3, 2);
+        firstNameMessage = new Text();
         Label lastName = new Label("Etternavn:");
-        mainPane.add(lastName, 1, 4);
         lastNameField = new TextField();
-        mainPane.add(lastNameField, 2, 4);
-        lastNameMessage = new Label();
-        mainPane.add(lastNameMessage, 3, 2);
+        lastNameMessage = new Text();
         Text adress = new Text("Adresse");
-        mainPane.add(adress, 1, 5);
         Label street = new Label("Gate:");
-        mainPane.add(street, 1, 6);
         streetField = new TextField();
-        mainPane.add(streetField, 2, 6);
-        streetMessage = new Label();
-        mainPane.add(streetMessage, 3, 2);
+        streetMessage = new Text();
         Label zipCode = new Label("Postboks:");
-        mainPane.add(zipCode, 1, 7);
         zipCodeField = new TextField();
-        mainPane.add(zipCodeField, 2, 7);
-        zipCodeMessage = new Label();
-        mainPane.add(zipCodeMessage, 3, 2);
+        zipCodeMessage = new Text();
         Label city = new Label("By:");
-        mainPane.add(city, 1, 8);
         cityField = new TextField();
-        mainPane.add(cityField, 2, 8);
-        cityMessage = new Label();
-        mainPane.add(cityMessage, 3, 2);
+        cityMessage = new Text();
         Text contact = new Text("Kontakt");
-        mainPane.add(contact, 1, 9);
         Label email = new Label("E-post:");
-        mainPane.add(email, 1, 10);
         emailField = new TextField();
-        mainPane.add(emailField, 2, 10);
-        emailMessage = new Label();
-        mainPane.add(emailMessage, 3, 2);
+        emailMessage = new Text();
         Label phone = new Label("Telefonnummer:");
-        mainPane.add(phone, 1, 11);
-        phoneField = new TextField();
-        mainPane.add(phoneField, 2, 11);
-        phoneMessage = new Label();
-        mainPane.add(phoneMessage, 3, 2);
+        phoneField = new TextField();     
+        phoneMessage = new Text();      
         registerButton = new Button("Registrer");
         HBox hbBtn = new HBox();
         hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
         hbBtn.getChildren().add(registerButton);
-        mainPane.add(hbBtn, 2, 12);
         resultText = new Text();
+        
+        mainPane.add(registerT, 1, 1);
+        
+        mainPane.add(position, 1, 2);
+        mainPane.add(positionCombo, 2, 2);
+        
+        mainPane.add(personalNumber, 1, 3);
+        mainPane.add(personalNumberField, 2, 3);
+        mainPane.add(personalNumberMessage, 3, 3);
+        
+        mainPane.add(firstName, 1, 4);
+        mainPane.add(firstNameField, 2, 4);
+        mainPane.add(firstNameMessage, 3, 4);
+        
+        mainPane.add(lastName, 1, 5);
+        mainPane.add(lastNameField, 2, 5);
+        mainPane.add(lastNameMessage, 3, 5);
+        
+        mainPane.add(adress, 1, 6);
+        
+        mainPane.add(street, 1, 7);
+        mainPane.add(streetField, 2, 7);
+        mainPane.add(streetMessage, 3, 7);
+        
+        mainPane.add(zipCode, 1, 8);
+        mainPane.add(zipCodeField, 2, 8);
+        mainPane.add(zipCodeMessage, 3, 8);
+        
+        mainPane.add(city, 1, 9);
+        mainPane.add(cityField, 2, 9);
+        mainPane.add(cityMessage, 3, 9);
+        
+        mainPane.add(contact, 1, 10);
+        
+        mainPane.add(email, 1, 11);
+        mainPane.add(emailField, 2, 11);
+        mainPane.add(emailMessage, 3, 11);
+        
+        mainPane.add(phone, 1, 12);
+        mainPane.add(phoneField, 2, 12);
+        mainPane.add(phoneMessage, 3, 12);
+        
+        mainPane.add(hbBtn, 2, 13);
+        // column span of 3, row span of 2:
+        mainPane.add(resultText, 1, 14, 3, 2);
         
     }
     
@@ -154,6 +171,10 @@ public class EmployeeRegistration extends Application {
         return mainPane;
     }
 
+    public String getPositionComboValue() {
+        return positionCombo.getValue().toString();
+    }
+    
     /**
      * @return the personalNumberField
      */
@@ -264,6 +285,10 @@ public class EmployeeRegistration extends Application {
      */
     public void setPhoneMessage(String phoneMessage) {
         this.phoneMessage.setText(phoneMessage);
+    }
+    
+    public void setResultText(String resultText) {
+        this.resultText.setText(resultText);
     }
     
 }

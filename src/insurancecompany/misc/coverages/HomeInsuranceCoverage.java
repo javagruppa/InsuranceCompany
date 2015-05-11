66e96b83d2cdbab9d8c4242462a9ce5c6a10b9d2
zlokex <andre.hovda@gmail.com>
2015-05-11 22:49:58 +0200

    HomeInsuranceCoverage updated

--- src/insurancecompany/misc/coverages/HomeInsuranceCoverage.java
+++ src/insurancecompany/misc/coverages/HomeInsuranceCoverage.java
@@ -5,6 +5,7 @@
  */
 package insurancecompany.misc.coverages;
 
+import insurancecompany.misc.damages.Damage;
 import insurancecompany.misc.damages.HomeInsuranceDamages;
 import java.util.List;
 
@@ -13,15 +14,45 @@ import java.util.List;
  * @author André
  */
 public enum HomeInsuranceCoverage {
-    PLUS, BASIC;
-    
-    public List<String> damages() {
-        switch(this) {
-            case PLUS: return HomeInsuranceDamages.PLUS;
-            case BASIC: return HomeInsuranceDamages.BASIC;
-            default: throw new IllegalArgumentException();
+    BASIC {
+        @Override
+        public Damage[] damages() {
+            Damage[] damages = {
+                Damage.FULL_VALUE_GUARANTEE,
+                Damage.BUILDING_DAMAGE,
+                Damage.TOTAL_REENTRY_OVER_SEVENTYFIVE,
+                Damage.GARDEN_ARTICLES,
+                Damage.REBUILDING_FOR_WHEELCHAIR_USER,
+                Damage.LOSS_OF_RENT,
+                Damage.LEGAL_LIABILITIES,
+                Damage.LEGAL_AID,
+                Damage.RODENT_DAMAGE
+            };
+            return damages;
         }
-    }
+    }, PLUS {
+        @Override
+        public Damage[] damages() {
+            Damage[] damages = {
+                Damage.FULL_VALUE_GUARANTEE,
+                Damage.BUILDING_DAMAGE,
+                Damage.TOTAL_REENTRY_OVER_SEVENTYFIVE,
+                Damage.GARDEN_ARTICLES,
+                Damage.REBUILDING_FOR_WHEELCHAIR_USER,
+                Damage.LOSS_OF_RENT,
+                Damage.LEGAL_LIABILITIES,
+                Damage.LEGAL_AID,
+                Damage.RODENT_DAMAGE,
+                Damage.FUNGUS_AND_ROT_DAMAGE,
+                Damage.PEST_CONTROL,
+                Damage.CONSEQUENTIAL_DAMAGE_AFTER_LEAK,
+                Damage.CONSEQUENTIAL_DAMAGE_AFTER_CRAFTS_ERROR
+            };
+            return damages;
+        }
+    };
+    
+    public abstract Damage[] damages();
     
     @Override
     public String toString() {

031d991f0a861a1416ec0cbd125dc583f742636f
zlokex <andre.hovda@gmail.com>
2015-05-11 22:49:41 +0200

    HomeContentInsuranceCoverage updated

---
+++ src/insurancecompany/misc/coverages/HomeContentInsuranceCoverage.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package insurancecompany.misc.coverages;
+
+import insurancecompany.misc.damages.Damage;
+import insurancecompany.misc.damages.HomeInsuranceDamages;
+import java.util.List;
+
+/**
+ *
+ * @author André
+ */
+public enum HomeContentInsuranceCoverage {
+    BASIC {
+        @Override
+        public Damage[] damages() {
+            Damage[] damages = {
+                Damage.FIRE_DAMAGE,
+                Damage.WATER_DAMAGE,
+                Damage.THEFT,
+                Damage.NATURAL_DISASTER,
+                Damage.BAG_SNATCHING,
+                Damage.ROBBERY_AND_ASSAULT,
+                Damage.SUBSISTENCE_EXPENSE,
+                Damage.LEGAL_LIABILITIES,
+                Damage.LEGAL_AID
+            };
+            return damages;
+        }
+    }, PLUS {
+        @Override
+        public Damage[] damages() {
+            Damage[] damages = {
+                Damage.FIRE_DAMAGE,
+                Damage.WATER_DAMAGE,
+                Damage.THEFT,
+                Damage.NATURAL_DISASTER,
+                Damage.BAG_SNATCHING,
+                Damage.ROBBERY_AND_ASSAULT,
+                Damage.SUBSISTENCE_EXPENSE,
+                Damage.LEGAL_LIABILITIES,
+                Damage.LEGAL_AID,
+                Damage.ACCIDENTAL_INJURY,
+                Damage.MOVING_INSURANCE,
+                Damage.ID_ANTITHEFT
+            };
+            return damages;
+        }
+    };
+    
+    public abstract Damage[] damages();
+    
+    @Override
+    public String toString() {
+        switch(this) {
+            case PLUS: return "Pluss";
+            case BASIC: return "Basis";
+            default: throw new IllegalArgumentException();
+        }
+    }
+    
+    public int getPricing() {
+        switch(this) {
+            case PLUS: return 1000;
+            case BASIC: return 0;
+            default: throw new IllegalArgumentException();
+        }
+    }
+    
+    
+        
+}

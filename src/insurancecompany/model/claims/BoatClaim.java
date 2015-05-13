32edf0a147a77ecf940f7c8421692c73d1767a95
carlmollen <Carl.mollen@gmail.com>
2015-05-13 17:05:40 +0200

    boatclaim tostring og javadoc

--- src/insurancecompany/model/claims/BoatClaim.java
+++ src/insurancecompany/model/claims/BoatClaim.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package insurancecompany.model.claims;
 
 import insurancecompany.misc.coverages.Damage;
@@ -13,18 +8,41 @@ import java.util.Date;
 import java.util.Set;
 
 /**
- *
+ * Boat claim class. Sub class to claim. All claims regarding boats are
+ * registered as boat claims
  * @author André
  */
 public class BoatClaim extends Claim implements Serializable {
+    /** SerialVersionUID used to identify this class for object IO */
     private static final long serialVersionUID = 1L;
 
+    /**
+     * Constructs a boatclaim with the specified parameters
+     * 
+     * @param customerId the customerId of the customer who owns the isnurance
+     * @param insuranceId the Id of the insurance this claim is covered by
+     * @param description the description of the damage
+     * @param dateHappened the date the damage happened
+     * @param damages the damage(s)
+     * @param appraisal the appraisal for the damage
+     */
     public BoatClaim(int customerId, int insuranceId, String description, 
             Calendar dateHappened, Set<Damage> damages, int appraisal) {
         super(customerId, insuranceId, description, dateHappened, damages, 
                 appraisal);
     }    
     
+    /**
+     * Constructs a boatclaim with the specified parameters, with image
+     * 
+     * @param customerId the customerId of the customer who owns the isnurance
+     * @param insuranceId the Id of the insurance this claim is covered by
+     * @param description the description of the damage
+     * @param dateHappened the date the damage happened
+     * @param damages the damage(s)
+     * @param appraisal the appraisal for the damage
+     * @param image image of the damage
+     */
     public BoatClaim(int customerId, int insuranceId, String description, 
             Calendar dateHappened, Set<Damage> damages, int appraisal, 
             Image image) {
@@ -32,12 +50,13 @@ public class BoatClaim extends Claim implements Serializable {
                 appraisal, image);
     }
 
-
-
-    
+    /**
+     * Returns a string representation of this claim
+     * @return a string representation of this claim
+     */
     @Override
     public String toString() {
-        String text = "Båtskademelding\n";
+        String text = "SKADEMELDING BÅT\n";
         text += super.toString();
         return text;
     }

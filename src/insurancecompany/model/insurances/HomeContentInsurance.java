72cec3ff7e47c08e6a79bf1a94310a03a8582768
carlmollen <Carl.mollen@gmail.com>
2015-05-13 18:42:51 +0200

    fikset

--- src/insurancecompany/model/insurances/HomeContentInsurance.java
+++ src/insurancecompany/model/insurances/HomeContentInsurance.java
@@ -18,6 +18,7 @@ import java.io.Serializable;
  * @author Carl
  */
 public class HomeContentInsurance extends PropertyInsurance implements Serializable {
+    /** SerialVersionUID used to identify this class for object IO */
     private static final long serialVersionUID = 1L;
     
     /** the home this insurance is for */

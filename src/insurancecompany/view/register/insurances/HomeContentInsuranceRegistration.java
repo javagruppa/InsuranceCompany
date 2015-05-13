0f4d86debd2143823ab5ea32bd1662a225d64ac4
Sindre Beba <sindre.beba@gmail.com>
2015-05-13 14:55:39 +0200

    HolidayHomeContentInsuranceRegistration and HomeContentInsuranceRegistration: Added.

---
+++ src/insurancecompany/view/register/insurances/HomeContentInsuranceRegistration.java
@@ -0,0 +1,13 @@
+/*
+ * Here comes the text of your license
+ * Each line should be prefixed with  * 
+ */
+package insurancecompany.view.register.insurances;
+
+/**
+ *
+ * @author Sindre
+ */
+public class HomeContentInsuranceRegistration {
+    
+}

1cd8141cf48d6dc4d771ea5f22f05bc2f7839bd9
Sindre Beba <sindre.beba@gmail.com>
2015-05-13 12:17:47 +0200

    xClaimRegistration: Created the remaining four.

---
+++ src/insurancecompany/view/register/claims/BoatClaimRegistration.java
@@ -0,0 +1,13 @@
+/*
+ * Here comes the text of your license
+ * Each line should be prefixed with  * 
+ */
+package insurancecompany.view.register.claims;
+
+/**
+ *
+ * @author Sindre
+ */
+public class BoatClaimRegistration {
+    
+}

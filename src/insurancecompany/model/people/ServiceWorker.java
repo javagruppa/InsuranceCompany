637ffdfec0c2095e871cd32f4738bad727e0a056
carlmollen <Carl.mollen@gmail.com>
2015-05-13 14:33:00 +0200

    serviceworker finished comments

--- src/insurancecompany/model/people/ServiceWorker.java
+++ src/insurancecompany/model/people/ServiceWorker.java
@@ -1,19 +1,25 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package insurancecompany.model.people;
 import insurancecompany.model.properties.Address;
 import java.io.Serializable;
 /**
- *
+ * ServiceWorker class. This is an employee of the insurance company.
+ * The service worker registers and views customers, insurances and claims.
+ * 
  * @author Carl
  */
 public class ServiceWorker extends Employee implements Serializable {
     private static final long serialVersionUID = 1L;
 
+    /**
+     * Constructs a ServiceWorker with the specified parameters
+     * 
+     * @param firstname the first name of this service worker
+     * @param lastname the last name of this service worker
+     * @param personalNumber the personal number of this service worker 
+     * @param email the email address of this service worker
+     * @param address the address of this service worker
+     * @param phone the phone number of this service worker
+     */
     public ServiceWorker(String firstname, String lastname, String personalNumber, String email, Address address, String phone) {
         super(firstname, lastname, personalNumber, email, address, phone);
     }

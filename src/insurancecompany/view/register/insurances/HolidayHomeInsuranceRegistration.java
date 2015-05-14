7ee6bbbb8bd21fa23529e767323f42653fe0940b
Sindre Beba <sindre.beba@gmail.com>
2015-05-14 19:48:01 +0200

    xInsuranceRegistration: Fixed imports.

--- src/insurancecompany/view/register/insurances/HolidayHomeInsuranceRegistration.java
+++ src/insurancecompany/view/register/insurances/HolidayHomeInsuranceRegistration.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package insurancecompany.view.register.insurances;
 
 import insurancecompany.misc.coverages.HolidayHomeInsuranceCoverage;
@@ -16,7 +11,6 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
@@ -89,7 +83,6 @@ public class HolidayHomeInsuranceRegistration {
         
         // Sets up the mainPane:
         mainPane = new GridPane();
-        mainPane.setAlignment(Pos.CENTER);
         // Sets CSS ID:
         mainPane.setId("innerPane");
         // Sets up column constraints. Width in pixels:

6342d42801dea160edabb3cd267582bdac8aa1b7
Sindre Beba <sindre.beba@gmail.com>
2015-05-14 16:26:17 +0200

    CarInsurance: Cleaned up. Fixed some commenting and coding so they follow our conventions.

--- src/insurancecompany/model/insurances/CarInsurance.java
+++ src/insurancecompany/model/insurances/CarInsurance.java
@@ -6,75 +6,76 @@ import insurancecompany.misc.coverages.CarInsuranceCoverage;
 import java.util.Calendar;
 import java.io.Serializable;
 
-
 /**
  * CarInsurance class. This is the insurances for cars.
  * 
+ * @author Carl
  * @author Sindre
  */
 public class CarInsurance extends Insurance implements Serializable {
-    /** SerialVersionUID used to identify this class for object IO */
+    
+    /** SerialVersionUID used to identify this class for object IO. */
     private static final long serialVersionUID = 1L;
     /** The bonus of this insurance. Used to calculate the yearly insurance 
-     * premium.
-     */
+     * premium. */
     private int bonus;
     /** The car this insurance is for. */
     private Car car;
     /** The coverage of this insurance. */
     private CarInsuranceCoverage coverage;
     /** Whether the car this insurance is for has a garage or not. */
-    private boolean hasGarage;
+    private boolean garage;
+    /** The date of when the bonus of this insurance was last updated. */
+    private Calendar lastBonusUpdate;
     /** The maximum driving length for this insurance. */
     private int maxLength;
+    /** The number of consecutive years the bonus has stayed at 70. */
+    private int yearsOnSeventy;
+    /** The number of consecutive years the bonus has stayed at 75. */
+    private int yearsOnSeventyFive; 
     /** Whether a person under 25 years is allowed to drive the car this 
      * insurance is for. */
     private boolean youngDriver;
-    /** The date of when the bonus of this insurance was last updated. */
-    private Calendar lastBonusUpdate;
-    /** The number of consecutive years the bonus has stayed at 70 */
-    private int yearsOnSeventy;
-    /** The number of consecutive years the bonus has stayed at 75 */
-    private int yearsOnSeventyFive; 
-    
     
     /**
-     * Constructs a new car insurance with the specified car, coverage, 
-     * customerId, excess, hasAlarm, hasGarage, maxLengh and youngDriver. Active 
-     * is set to true. Bonus is set to 0. Date is set to the current date. 
-     * InsuranceId is automatically set to nextInsuranceId.
+     * Constructs a new car insurance with the specified parameters.
+     * Active is set to true. Bonus is set to 0. Date is set to the current 
+     * date. InsuranceId is automatically set to nextInsuranceId.
      * 
-     * @param car the car this insurance is for
-     * @param coverage the coverage of this insurance
-     * @param customerId the id of the customer who owns this insurance
-     * @param excess the excess of this insurance
+     * @param car The car this insurance is for.
+     * @param coverage The coverage of this insurance.
+     * @param customerId The id of the customer who owns this insurance.
+     * @param excess The excess of this insurance.
      * @param existingBonus The existing bonus of the customer.
-     * @param hasGarage whether the car this insurance is for has a garage or 
-     * not
-     * @param maxLength the maximum driving length for this insurance
-     * @param youngDriver whether a person under 25 years is allowed to drive 
-     * the car this insurance is for
+     * @param garage Whether the car this insurance is for has a garage or not.
+     * @param maxLength The maximum driving length for this insurance.
+     * @param youngDriver Whether a person under 25 years is allowed to drive 
+     * the car this insurance is for.
      */
     public CarInsurance(Car car, CarInsuranceCoverage coverage, int customerId, 
-            int excess, int existingBonus, boolean hasGarage, int maxLength, 
+            int excess, int existingBonus, boolean garage, int maxLength, 
             boolean youngDriver) {
         super(customerId, excess);
         this.bonus = existingBonus;
         this.car = car;
         this.coverage = coverage;
-        this.hasGarage = hasGarage;
+        this.garage = garage;
         this.maxLength = maxLength;
         this.youngDriver = youngDriver;
-        
-        /* Initializes last bonus update to the date of when the insurance
-        was created, using the super class getDate() */
+        // Initializes last bonus update to the date of when the insurance
+        // was created, using the super class getDate().
         lastBonusUpdate = super.getDate();
     }
     
-    /**
-     * Returns the type of this insurance in form of a String.
-     * @return the insurance type
-     */
+    // GET METHODS
+    
+    /** @return The coverage of this insurance. */
+    @Override
+    public CarInsuranceCoverage getCoverage() {
+        return coverage;
+    }
+    
+    /** @return The type of insurance in form of a String. */
     @Override
     public String getName() {
         return "Bilforsikring";
@@ -96,55 +97,56 @@ public class CarInsurance extends Insurance implements Serializable {
         result.append("BILFORSIKRING");
         result.append("\n").append(super.toString());
         result.append("\nType: ").append(coverage.toString());
-        result.append("\nGarasje: ").append(hasGarage ? "Ja" : "Nei");
+        result.append("\nGarasje: ").append(garage ? "Ja" : "Nei");
         result.append("\nKan personer under 25 kjøre bilen: ").
                 append(youngDriver ? "Ja" : "Nei");
         result.append("\nMaksimum kjørelengde: ").append(maxLength);
         result.append("\nBonus: ").append(bonus).append("%");
-        result.append("\nBonus sist endret: ").append(DateUtility.NORWEGIAN_DATE_FORMAT.format(lastBonusUpdate));
+        result.append("\nBonus sist endret: ").append(DateUtility.
+                NORWEGIAN_DATE_FORMAT.format(lastBonusUpdate));
         // Returns the string.
         return result.toString();
-
     }
+    
+    // CALCULATE PREMIUM METHODS
     
     /**
      * Calculates and returns the price for the maximum number of kilometers
      * included in this insurance.
-     * @return the maxLengthCost of this insurance.
+     * 
+     * @return The maxLengthCost of this insurance.
      */
-    public int maxlengthCost(){
+    public int maxlengthCost() {
 	return maxLength / 10;
     }
 
     /**
      * Calculates and returns the price drop of this insurance due the chosen
      * excess.
-     * @return the price drop of this insurance based on the excess
+     * 
+     * @return The price drop of this insurance based on the excess.
      */
-    public int excessCalculator(){
+    public int excessCalculator() {
 	// Finds the excess set for this insurance
         int excess = getExcess();
         // The price drop based on the set excess of this insurance
         int excessSaving = 0;
-        // If excess is set to 0, the excess saving will be set to -2000, and
-        // there will be 2000 added to the price
-	if(excess == 0){
-	excessSaving = -2000;
-        }
-        // With excess set between 0 and 5000, the excess saving will be
-        // calulated to 20& of the excess
-	else if(excess > 0 && excess <= 5000){
-	excessSaving = excess / 5;
-        }
-        // With excess set between 5000 and 10000, the excess saving will be
-        // calulated to 25& of the excess
-	else if(excess > 5000 && excess <= 10000){
-	excessSaving = excess / 4;
-        }
-        // With excess set above 10000, the excess saving will be
-        // calulated to 33& of the excess
-	else if(excess > 10000){
-	excessSaving = excess / 3;
+	if (excess == 0) {
+            // If excess is set to 0, the excess saving will be set to -2000, 
+            // and there will be 2000 added to the price
+            excessSaving = -2000;
+        } else if (excess > 0 && excess <= 5000) {
+            // With excess set between 0 and 5000, the excess saving will be
+            // calulated to 20% of the excess
+            excessSaving = excess / 5;
+        } else if (excess > 5000 && excess <= 10000) {
+            // With excess set between 5000 and 10000, the excess saving will 
+            // be calulated to 25% of the excess
+            excessSaving = excess / 4;
+        } else if (excess > 10000) {
+            // With excess set above 10000, the excess saving will be
+            // calulated to 33% of the excess
+            excessSaving = excess / 3;
         }
         // returns the price drop
         return excessSaving;
@@ -155,34 +157,32 @@ public class CarInsurance extends Insurance implements Serializable {
      * parameters including whether the customer fullfills certain aspects:
      * youngdriver, garage, alarm.
      */
-    public void premiumCalculator(){
+    public void premiumCalculator() {
 	double youngDriverMultiplicator = 1.0;
 	double garageMultiplicator = 1.0;
 	double alarmMultiplicator = 1.0;
         int bonusMultiplicator = bonus / 100;
         // If there will be a young driver of this insured car, the
         // multiplicator is set to 1.2
-	if (youngDriver){
-	youngDriverMultiplicator = 1.2;
+	if (youngDriver) {
+            youngDriverMultiplicator = 1.2;
         }
         // If the car of this insurance has an alam, the alarm multiplicator
         // is set to 0.8
-	if (car.getAlarm()){
-	alarmMultiplicator = 0.8;
+	if (car.getAlarm()) {
+            alarmMultiplicator = 0.8;
         }
         // If the car of this insurance has a garage, the garage multiplicator
         // is set to 0.8
-	if (hasGarage){
-	garageMultiplicator = 0.8;
+	if (garage) {
+            garageMultiplicator = 0.8;
         }
-
 	// Adds up the total of the multiplicators above
         double allMultiplicators = garageMultiplicator + alarmMultiplicator +
                 youngDriverMultiplicator;
 	// Divides the sum of the multiplicators by 3, getting an average
         // multiplicator that is used to calculate the total premium.
         double totalMultiplicator = allMultiplicators / 3;
-
 	// Gets the cost of this type of insurance (Casco, partly casco or
         // Liability)
         int typeCost = coverage.getPricing();	
@@ -199,9 +199,9 @@ public class CarInsurance extends Insurance implements Serializable {
         int setPremium = (int)newPremium - (int)bonusDecrease;
         // Changes the premium of this insurance.
         setPremium(setPremium);
-
     }
     
+    // BONUS METHODS
     
     /**
      * Drops the bonus in case of an accident.
@@ -237,12 +237,12 @@ public class CarInsurance extends Insurance implements Serializable {
             yearsOnSeventy = 0;
             yearsOnSeventyFive = 0;
         // Once bonus has reached 70 points, we start counting consecutive
-        //years on seventy. This affects the bonus drops
+        // years on seventy. This affects the bonus drops.
         } else if (bonus == 70 && yearsOnSeventy < 5) {
             yearsOnSeventyFive = 0;
             yearsOnSeventy++;
         // When 5 consecutive years with 70 as a bonus, bonus goes to 75,
-        //and we start counting consecutive years at this stage aswell.
+        // and we start counting consecutive years at this stage aswell.
         } else if (bonus == 70 && yearsOnSeventy == 5) {
             bonus = 75;
             yearsOnSeventyFive++;
@@ -262,90 +262,5 @@ public class CarInsurance extends Insurance implements Serializable {
         for (int i = 0; i < years; i++) {
             yearlyBonusIncrease();
         }
-    }
-    
-    /**
-     * Returns the coverage of this insurance.
-     * @return the coverage
-     */
-    @Override
-    public Object getCoverage() {
-        return coverage;
-    }
-    
-    /**
-     * Returns the boolean representing whether the insurance covers
-     * young drivers.
-     * 
-     * @return the boolean youngDriver
-     */
-    public boolean getYoungDriver() {
-        return youngDriver;
-    }
-    
-    
-    /**
-     * Returns the bonus of this car insurance.
-     * @return the bonus of this insurance
-     */
-    public int getBonus() {
-        return bonus;
-    }
-    
-    /**
-     * Sets the bonus of this car insurance, and calculates the new premium
-     * based on the new information.
-     * @param bonus to be set
-     */
-    public void setBonus(int bonus) {
-        this.bonus = bonus;
-        // Calculates and sets new premium
-        premiumCalculator();
-    }
-    
-    /**
-     * Sets the max length for the car to this car insurance, and updates the
-     * premium of the insurance.
-     * @param maxLength the maxlenth to be set
-     */
-    public void setMaxLength(int maxLength) {
-        this.maxLength = maxLength;
-        // Updates the premium based on the new maxlength
-        premiumCalculator();
-    }
-    
-    /**
-     * Returns the max length for the car to this car insurance.
-     * @return the max length
-     */
-    public int getMaxLength() {
-        return maxLength;
-    }
-    
-    /**
-     * Sets a true or false to the has garage option of this insurance, and
-     * updates the premium of this insurance.
-     * @param hasGarage to be set
-     */
-    public void setHasGarage(boolean hasGarage) {
-        this.hasGarage = hasGarage;
-        // Updates the premium based on the new information
-        premiumCalculator();
-    }
-    
-    /**
-     * Returns a true or false to the has garage option of this insurance.
-     * @return the boolean hasGarage
-     */
-    public boolean getHasGarage() {
-        return hasGarage;
-    }
-    
-    /**
-     * Returns the car of this car insurance.
-     * @return the car
-     */
-    public Car getCar() {
-        return car;
     }
 }

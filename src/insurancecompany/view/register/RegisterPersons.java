fd56bafe741b969d02a44cc86caf7b7c5483e069
Sindre Beba <sindre.beba@gmail.com>
2015-05-18 22:06:01 +0200

    RegisterPersons: Error checked, updated and added comments.

--- src/insurancecompany/view/register/RegisterPersons.java
+++ src/insurancecompany/view/register/RegisterPersons.java
@@ -1,72 +1,56 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package insurancecompany.view.register;
 
 import insurancecompany.view.register.persons.RegisterCustomer;
-import java.util.ArrayList;
-import java.util.List;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.ContentDisplay;
-import javafx.scene.control.ScrollPane;
 import javafx.scene.control.ToggleButton;
 import javafx.scene.control.ToggleGroup;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 /**
- *
+ * This class creates the graphical user interface (GUI) for registration of 
+ * persons. It creates a pane which is sent to the controller and 
+ * thereafter displayed.
+ * 
+ * It consists of a side toolbar and a center part where another pane is placed.
+ * 
  * @author André
+ * @author Sindre
  */
 public class RegisterPersons {
     
-    private RegisterCustomer customerRegistration;
-    
-    private Pane customerRegistrationPane;
-    
-    private Scene scene;
+    // Declaration of the main pane which is sent to the controller.
     private BorderPane mainPane;
     
+    // Declaration of all the nodes in the side toolbar.
     private Pane sideToolBarPane;
-    
     private ToggleButton customerButton;
     private ToggleButton employeeButton;
     private ToggleGroup toggleGroup;
-            
-    //public static void main(String[] args) {
-    //    launch(args);
-    //}
     
-    public void start(Stage stage) throws Exception {
-        show(stage);
-    }
-    
-    public void show(Stage stage) {
-        stage.setTitle("Kunderegistrering");
-        stage.setScene(scene);
-        stage.show();
-    }
-    
+    /**
+     * Default constructor. Initializes all field and sets up the view.
+     */
     public RegisterPersons() {
+        
+        // Initialization of the pane.
         mainPane = new BorderPane();
+        
+        // Creates and adds the side toolbar.
         sideToolBarPane = createSideToolBar();
         mainPane.setLeft(sideToolBarPane);
-        scene = new Scene(mainPane, 800, 600);
-        //initializeViews();  
     }
     
+    /* Creates the side toolbar. */
     private VBox createSideToolBar() {
         VBox vbox = new VBox();
         vbox.setPrefWidth(160);
@@ -77,14 +61,13 @@ public class RegisterPersons {
         customerButton.setId("sideToolbarButton");
         Image customerImage = new Image("insurancecompany/resources/images/customer.png");
         customerButton.setGraphic(new ImageView(customerImage));
-        //carInsuranceButton.setContentDisplay(ContentDisplay.LEFT);
         
         employeeButton = new ToggleButton("Ny ansatt");
         employeeButton.setId("sideToolbarButton");
         Image employeeImage = new Image("insurancecompany/resources/images/employee.png");
         employeeButton.setGraphic(new ImageView(employeeImage));
         
-        // Set up togglegroup and connect it to all togglebuttons:
+        // Sets up the ToggleGroup and connects all ToggleButtons.
         toggleGroup = new ToggleGroup();
         customerButton.setToggleGroup(toggleGroup);
         employeeButton.setToggleGroup(toggleGroup);
@@ -95,22 +78,31 @@ public class RegisterPersons {
             b.setMinWidth(Button.USE_PREF_SIZE);
             b.setMaxWidth(Double.MAX_VALUE);
         });
-        
                 
         vbox.getChildren().addAll(buttons);
        
         return vbox;
     }
     
+    /** @return The main pane of this class. */
     public BorderPane getMainPane() {
         return mainPane;
     }
 
-    
+    /**
+     * Sets the event handler for the customerButton.
+     * 
+     * @param value The event handler to set.
+     */
     public void setCustomerButtonEventHandler(EventHandler<ActionEvent> value) {
         customerButton.setOnAction(value);
     }
     
+    /**
+     * Sets the event handler for the employeeButton.
+     * 
+     * @param value The event handler to set.
+     */
     public void setEmployeeButtonEventHandler(EventHandler<ActionEvent> value) {
         employeeButton.setOnAction(value);
     }

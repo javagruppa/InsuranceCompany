1b1acc8622ada3e1a0664c2c2ef6524c0835cc0a
Sindre Beba <sindre.beba@gmail.com>
2015-05-18 22:40:00 +0200

    ClaimRegister: Error checked, updated, removed unused methods and added comments.

--- src/insurancecompany/model/datastructures/ClaimRegister.java
+++ src/insurancecompany/model/datastructures/ClaimRegister.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package insurancecompany.model.datastructures;
 
 import insurancecompany.misc.coverages.Damage;
@@ -11,65 +6,45 @@ import java.io.*;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
+
 /**
- *
+ * This class creates the register for claims. It consists of a HashSet with all
+ * the claims as well as several methods to manipulate and get information from
+ * the register.
+ * 
  * @author André
+ * @author Sindre
  */
 public class ClaimRegister {
-    
+    /** The file path of the file where the claims are saved. */
     private static String claimsFilePath = "src/insurancecompany/resources/datastructures/claimSet.dta";
-    
+    /** The set of claims. */
     private Set<Claim> claims;
     
     /**
-     * Constructor:
+     * Default constructor. Initializes the set.
      */
     public ClaimRegister() {
-        claims = new HashSet<Claim>();
+        claims = new HashSet<>();
     }
     
     /**
      * Adds a new claim to this register if it does not already exist.
-     * @param claim claim to be added to the register
-     * @return 
+     * 
+     * @param claim Claim to be added to the register.
+     * @return True if this list changed as a result of the call.
      */
     public boolean addClaim(Claim claim) {
         return claims.add(claim);
     }
     
     /**
-     * Finds and returns a list of claims matching the owner of given customer id.
-     * @param customerId customer id of a claim owner
-     * @return a list of claims matching the owner of the customer id
-     */
-    public List<Claim> findClaimsByCustomerId(int customerId) {
-        // Creates an ArrayList which will be returned at the end of the method.
-        List<Claim> result = new ArrayList<Claim>();
-        // Creates an iterator for the list:
-        Iterator<Claim> iterator = claims.iterator();
-        // Runs through the whole list:
-        while(iterator.hasNext()) {
-            Claim claim = iterator.next();
-            if (claim.getCustomerId() == customerId) {
-                result.add(claim);
-            }
-        }
-        // Returns null if no matches are found:
-        if (result.isEmpty()) {
-            return null;
-        } else {
-            // Returns the newly created list otherwise:
-            return result;
-        }
-    }
-    
-    /**
      * Finds and returns a claim based on claim id.
-     * @param claimId claim id of a claim in the list
-     * @return 
+     * 
+     * @param claimId Claim ID of the claim that the method looks for.
+     * @return The claim if it is found. Null otherwise.
      */
     public Claim getClaimById(int claimId) {
         for (Claim claim : claims) {
@@ -80,6 +55,23 @@ public class ClaimRegister {
         return null;
     }
     
+    /**
+     * Finds and returns a list of claims based on the parameters.
+     * 
+     * @param customerId The customer ID of the claims that the method looks 
+     * for. It's 0 if it's not part of the criteria.
+     * @param type The claim type of the claims that the method looks for. It's
+     * null if it's not part of the criteria.
+     * @param damage The damage of the claims that the method looks for. It's 
+     * null if it's not part of the criteria.
+     * @param insuranceId The insurance ID of the claims that the method looks 
+     * for. It's null if it's not part of the criteria.
+     * @param fromDate The earliest date the incident happened. It's null if 
+     * it's not part of the criteria.
+     * @param toDate The latest date the incident happened. It's null if it's
+     * not part of the criteria.
+     * @return A list of all the claims that match the criteria.
+     */
     public List<Claim> getClaims(int customerId, String type, Damage damage, 
             int insuranceId, Calendar fromDate, Calendar toDate) {
         List<Claim> result = new ArrayList<>();
@@ -96,72 +88,15 @@ public class ClaimRegister {
         }
         return result;
     }
-    
-    /**
-     * Returns the number of claims in this register.
-     * @return 
-     */
-    public int size() {
-        return claims.size();
-    }
-    
-    /**
-     * Returns all the claims between the two dates in the parameter.
-     * @param from
-     * @param to
-     * @return 
-     */
-    public List<Claim> claimsBetweenDates(Calendar from, Calendar to) {
-        List<Claim> result = new ArrayList<Claim>();
-        // Checks for every claim in this register:
-        for (Claim claim : claims) {
-            // The date of when the damage happened.
-            Calendar date = claim.getDateHappened();
-            // Checks if the claims date is inbetween the 2 dates specified in the parameter.
-            if (date.after(from) && date.before(to)) {
-                // Adds this claim to the return set:
-                result.add(claim);
-            }
-        }
-        // Returns a list containing all claims between correct dates:
-        return result;
-    }
-    
-    public List<Claim> claimTypesBetweenDate(Class<?> type, Calendar from, Calendar to) {
-        List<Claim> result = new ArrayList<Claim>();
-        // Checks for every claim in this register:
-        for (Claim claim : claims) {
-            // The date of when the damage happened:
-            Calendar date = claim.getDateHappened();
-            // Checks if the claims date is inbetween the 2 dates specified in the parameter:
-            if (date.after(from) && date.before(to)) {
-                // Checks if the claim is of the class type specified in the parameter:
-                if (type.isInstance(claim)) {
-                    // Adds this claim to the return set:
-                    result.add(claim);
-                }
-            }
-        }
-        // Returns a list containing all claims of specified type and between correct dates:
-        return result;
-    }
-    
-    public List<Claim> claimTypes(Class<?> type) {
-        List<Claim> result = new ArrayList<Claim>();
-        // Checks for every claim in this register:
-        for (Claim claim : claims) {
-            // Checks if the claim is of the class type specified in the parameter:
-            if (type.isInstance(claim)) {
-                // Adds this claim to the return set:
-                result.add(claim);
-            }
-        }
-        // Returns a list containing all claims of specified type:
-        return result;
+
+    /** @return A set of all the claims in the register. */
+    public Set<Claim> getClaims() {
+        return claims;
     }
     
     /**
      * Writes this registers set of claims to file.
+     * 
      * @throws IOException 
      */
     public void writeClaimsToFile() throws IOException{
@@ -170,8 +105,11 @@ public class ClaimRegister {
             oos.writeObject(claims);
         }
     }
+    
     /**
-     * Reads a set of claims from file and stores them in the set in this register.
+     * Reads a set of claims from file and stores them in the set in this 
+     * register.
+     * 
      * @throws IOException
      * @throws ClassNotFoundException 
      */
@@ -180,12 +118,5 @@ public class ClaimRegister {
                 new FileInputStream(claimsFilePath))) {
             claims = (HashSet<Claim>) ois.readObject();        
         }
-    }
-
-    /**
-     * @return the claims
-     */
-    public Set<Claim> getClaims() {
-        return claims;
     }
 }
